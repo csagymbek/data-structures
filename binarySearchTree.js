f44b7b35ca1341776787a05c3b08a0bb82c5b0eb
Serik <csagymbek@gmail.com>
2021-08-26 10:24:21 -0700

    initial commit

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -10,4 +10,37 @@ class BST {
   constructor(root = null) {
     this.root = root;
   }
+  add(val) {
+    if (!this.root) {
+      this.root = new Node(val);
+    } else {
+      const searchNode = (node) => {
+        if (val === node.val) {
+          return;
+        } else if (val !== node.val) {
+          if (val < node.val) {
+            if (node.left) {
+              return searchNode(node.left);
+            } else {
+              node.left = new Node(val);
+            }
+          } else if (val > node.val) {
+            if (node.right) {
+              return searchNode(node.right);
+            } else {
+              node.right = new Node(val);
+              return;
+            }
+          }
+        }
+      };
+      searchNode(this.root);
+    }
+  }
 }
+
+const newBST = new BST();
+newBST.add(0);
+newBST.add(-1);
+newBST.add(1);
+console.log(newBST);
